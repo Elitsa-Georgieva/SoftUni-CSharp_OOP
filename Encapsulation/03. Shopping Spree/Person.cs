9477151873029ea62e7cc82165123d5ea2175bbb
Elitsa-Georgieva <itsa.georgieva@gmail.com>
2021-07-06 16:49:59 +0300

    added 1 new program
    added 1 new program to Encapsulation folder

---
+++ Encapsulation/03. Shopping Spree/Person.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace _03.ShoppingSpree
+{
+    public class Person
+    {
+        private string name;
+        private decimal money;
+        private List<Product> bagOfProducts;
+
+        public Person(string name, decimal money)
+        {
+            this.Name = name;
+            this.Money = money;
+            this.bagOfProducts = new List<Product>();
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            private set
+            {
+                if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+                this.name = value;
+            }
+        }
+
+        public decimal Money
+        {
+            get
+            {
+                return this.money;
+            }
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                }
+                this.money = value;
+            }
+        }
+
+        public IReadOnlyCollection<Product> BagOfProducts
+        {
+            get
+            {
+                return this.bagOfProducts.AsReadOnly();
+            }
+            private set
+            {
+
+            }
+        }
+
+        public void AddProduct(Product product, Person person)
+        {
+            if (product.Price <= person.Money)
+            {
+                money -= product.Price;
+                bagOfProducts.Add(product);
+                Console.WriteLine($"{person.name} bought {product.Name}");
+            }
+            else
+            {
+                Console.WriteLine($"{person.Name} can't afford {product.Name}");
+            }
+        }
+    }
+}
---
+++ Encapsulation/03. Shopping Spree/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.ShoppingSpree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> persons = new List<Person>();
+            List<Product> products = new List<Product>();
+
+            try
+            {
+                string[] personsInfo = Console.ReadLine().Split(';');
+                for (int i = 0; i < personsInfo.Length; i++)
+                {
+                    string[] parts = personsInfo[i].Split('=');
+                    string name = parts[0];
+                    decimal money = decimal.Parse(parts[1]);
+
+                    Person person = new Person(name, money);
+                    persons.Add(person);
+                }
+
+                string[] productInfo = Console.ReadLine().Split(';', StringSplitOptions.RemoveEmptyEntries);
+                for (int i = 0; i < productInfo.Length; i++)
+                {
+                    string[] parts = productInfo[i].Split('=');
+                    string name = parts[0];
+                    decimal price = decimal.Parse(parts[1]);
+
+                    Product product = new Product(name, price);
+                    products.Add(product);
+                }
+
+            }
+            catch (ArgumentException ae)
+            {
+                Console.WriteLine(ae.Message);
+                return;
+            }
+
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "END")
+                {
+                    break;
+                }
+
+                string[] command = input.Split(" ");
+
+                string personName = command[0];
+                string productName = command[1];
+
+                Person currPerson = persons.FirstOrDefault(p => p.Name == personName);
+                Product currProduct = products.FirstOrDefault(p => p.Name == productName);
+                
+                currPerson.AddProduct(currProduct, currPerson);
+
+                
+            }
+            foreach (Person person in persons)
+            {
+                if (!person.BagOfProducts.Any())
+                {
+                    Console.WriteLine($"{person.Name} - Nothing bought");
+                    continue;
+                }
+
+                Console.WriteLine($"{person.Name} - {string.Join(", ", person.BagOfProducts)}");
+            }
+        }
+    }
+}
