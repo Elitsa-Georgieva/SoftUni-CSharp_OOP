6348fa27dcf0c78857538689506d434517e4d692
Elitsa-Georgieva <itsa.georgieva@gmail.com>
2021-07-06 14:45:34 +0300

    Added new folder Encapsulation
    Added 2 new programs

---
+++ Encapsulation/01.ClassBoxData/Box.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace _01.ClassBoxData
+{
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double length, double width, double height)
+        {
+            this.Length = length;
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public double Length
+        {
+            get
+            {
+                return length;
+            }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Length cannot be zero or negative.");
+                }
+                this.length = value;
+            }
+        }
+
+        public double Width
+        {
+            get
+            {
+                return width;
+            }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Width cannot be zero or negative.");
+                }
+                this.width = value;
+            }
+        }
+
+        public double Height
+        {
+            get
+            {
+                return height;
+            }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Height cannot be zero or negative.");
+                }
+                this.height = value;
+            }
+        }
+
+        /// <summary>
+        /// Volume = lwh
+        /// </summary>
+        /// <returns></returns>
+
+        public string CalcVolume()
+        {
+            double result = Length * Width * Height;
+            return $"Volume - {result:F2}";
+        }
+
+        public string CalcLateralSurfaceArea()
+        {
+            //Lateral Surface Area = 2lh + 2wh
+            double result = (2 * Length * Height) + (2 * Width * Height);
+            return $"Lateral Surface Area - {result:F2}";
+        }
+
+        public string CalcSurfaceArea()
+        {
+            //Surface Area = 2lw + 2lh + 2wh
+            double result = (2 * Length * Width) + (2 * Length * Height) + (2 * Width * Height);
+            return $"Surface Area - {result:F2}";
+        }
+    }
+}
---
+++ Encapsulation/01.ClassBoxData/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _01.ClassBoxData
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+
+            try
+            {
+                Box box = new Box(length, width, height);
+                Console.WriteLine(box.CalcSurfaceArea());
+                Console.WriteLine(box.CalcLateralSurfaceArea());
+                Console.WriteLine(box.CalcVolume());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
